ae33c257cb3997971f1460487ef3612808b137c0
Titan <howay.tan@gmail.com>
2016-10-17 17:44:48 +0800

    chang addUserInfo & refresh to a new style

--- src/processor.ts
+++ src/processor.ts
@@ -1,6 +1,9 @@
 import { Processor, Config, ModuleFunction, DoneFunction, rpc } from "hive-processor";
 import { Client as PGClient, ResultSet } from "pg";
 import { createClient, RedisClient } from "redis";
+import { servermap, triggermap } from "hive-hostmap";
+import * as nanomsg from "nanomsg";
+import * as msgpack from "msgpack-lite";
 import * as bunyan from "bunyan";
 import * as uuid from "node-uuid";
 
@@ -34,51 +37,65 @@ let config: Config = {
   addr: "ipc:///tmp/profile.ipc"
 };
 
+let user_trigger = nanomsg.socket("pub");
+user_trigger.bind(triggermap.group);
+
+
 let processor = new Processor(config);
 
 processor.call("refresh", (db: PGClient, cache: RedisClient, done: DoneFunction) => {
   log.info("refresh");
   db.query("SELECT id, openid, name, gender, identity_no, phone, nickname, portrait FROM users", [], (err: Error, result: ResultSet) => {
     if (err) {
-      log.error(err, "query error");
-      done();
-    }
-    let users = [];
-    for (let row of result.rows) {
-      users.push(row2user(row));
-    }
-    log.info("users:" + users);
-    let multi = cache.multi();
-    for (let user of users) {
-      multi.hset("profile-entities", user.id, JSON.stringify(user));
-      multi.lpush("profile", user.id);
-    }
-    multi.exec((err, replies) => {
-      if (err) {
-        log.info("multi err" + err);
+      log.error(err, "select users from db error");
+      return;
+    } else {
+      let users = result.rows.map(row => row2user(row));
+      let multi = cache.multi();
+      for (let user of users) {
+        multi.hset("profile-entities", user.id, JSON.stringify(user));
+        multi.lpush("profile", user.id);
       }
-      done();
-    });
+      multi.exec((err, replies) => {
+        if (err) {
+          log.info("multi err" + err);
+        }
+        done();
+      });
+    }
   });
 });
 
-processor.call("addUserInfo", (db: PGClient, cache: RedisClient, done: DoneFunction, openid: string, gender: string, nickname: string, portrait: string) => {
-  log.info("addUserInfo");
+processor.call("addUserInfo", (db: PGClient, cache: RedisClient, done: DoneFunction, openid: string, gender: string, nickname: string, portrait: string, callback: string) => {
+  log.info(`addUserInfo openid: ${openid}, gender: ${gender}, nickname: ${nickname}, portrait: ${portrait}, callback: ${callback}`);
   let uid = uuid.v1();
   db.query("INSERT INTO users (id, openid, gender, nickname, portrait) VALUES ($1, $2, $3, $4 ,$5)", [uid, openid, gender, nickname, portrait], (err: Error) => {
     if (err) {
-      log.error(err, "query error");
+      log.error(err, "insert into DB 'users' error!");
+      cache.setex(callback, 30, JSON.stringify({
+        code: 500,
+        msg: err.message
+      }));
     } else {
       let profile_entities = { id: uid, openid: openid, gender: gender, nickname: nickname, portrait: portrait };
       let multi = cache.multi();
       multi.hset("profile-entities", uid, JSON.stringify(profile_entities));
       multi.lpush("profile", uid);
+      multi.setex(callback, 30, JSON.stringify({
+        code: 200,
+        data: openid
+      }));
       multi.exec((err, replies) => {
         if (err) {
-          log.error(err);
+          log.error(err, "cache multi error!");
+          cache.setex(callback, 30, JSON.stringify({
+            code: 500,
+            msg: err.message
+          }));
         }
         done();
       });
+      // user_trigger.send(msgpack.encode({ uid }));
     }
   });
 });
